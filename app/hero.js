6b66ba313e672ac0a16aabd8f5730b4ff50b4904
ep0ke <fxroux@me.com>
2016-10-05 10:44:19 +0200

    Start of the project

---
+++ app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by ep0k on 03/10/16.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by ep0k on 03/10/16.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
